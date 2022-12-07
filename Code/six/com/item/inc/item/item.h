7c7e8efb8f0dead607609f8f1ec6976ee0148a7d
wangzilong <82163544+Tomorrow-SSS@users.noreply.github.com>
2022-12-07 16:45:25 +0800

    Delete Code/six/com directory

--- Code/six/com/item/inc/item/item.h
+++
@@ -1,9 +0,0 @@
-#ifndef COM_ITEM_INC_ITEM_ITEM_H_
-#define COM_ITEM_INC_ITEM_ITEM_H_
-
-#include "item_publisher.h"
-#include "item_subscription.h"
-#include "pub_to_sub.h"
-#include "ring_buffer.h"
-
-#endif /* COM_ITEM_INC_ITEM_ITEM_H_ */
